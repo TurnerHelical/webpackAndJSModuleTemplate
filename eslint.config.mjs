01b69cd36fb83197065e6753f9b20d2382949859
TurnerHelical <hunterde91@gmail.com>
2025-06-09 16:13:56 -0400

    fixed the errors in the config files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,14 @@
-import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig, globalIgnores } from "eslint/config";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-  globalIgnores([
-    "webpack.config.js"
-  ])
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: ["js"],
+    extends: ["js/recommended"],
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  globalIgnores(["webpack.config.js"])
 ]);
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,1 @@
-import "./styles.css";
+import "../CSS/style.css";
